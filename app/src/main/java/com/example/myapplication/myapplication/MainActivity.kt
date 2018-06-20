53a2977f40bd743c3b45e7075cd1ecac0b2f0d2c
William Peroche <williamp@wearexd.com>
2018-06-20 18:59:24 +1200

    first commit

---
+++ app/src/main/java/com/example/myapplication/myapplication/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.myapplication.myapplication
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.view.View
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun sendMessage(view: View) {
+        val editText = findViewById<EditText>(R.id.editText)
+        val message = editText.text.toString()
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
+    }
+}
+
